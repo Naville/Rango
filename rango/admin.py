c1e2d00b281b0eb25481238fa842029b8ea4fe64
Hansheng Zhang <403799106@qq.com>
2017-02-02 16:58:08 +0000

    Chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
+from rango.models import Category, Page,Question
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url','category')
+admin.site.register(Page, PageAdmin)
+admin.site.register(Category)
+admin.site.register(Question)
--- rango/models.py
+++ rango/models.py
@@ -5,6 +5,10 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128,unique=True)
+    views= models.IntegerField(default=0)
+    likes= models.IntegerField(default=0)
+    class Meta:
+        verbose_name_plural="Categories"
     def __str__(self):
         return self.name
     def __unicode__(self):
@@ -15,7 +19,23 @@ class Page(models.Model):
     title=models.CharField(max_length=128)
     url=models.URLField()
     views=models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural="Pages"
     def __str__(self):
         return self.title
     def __unicode__(self):
         return self.title
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
